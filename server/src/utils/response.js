7fdde1137c167b2f1968bd2400355dedd03bab8a
karishmahegde <karishmahegde@outlook.com>
2026-06-15 20:48:36 -0400

    Comments in response.js

--- server/src/utils/response.js
+++ server/src/utils/response.js
@@ -1,3 +1,9 @@
+//Why we need it: To return the response to the client.
+//How it works: It returns the response to the client.
+//What it returns: A function that can be used to return the response to the client.
+//How to use it: It can be used to return the response to the client.
+
+//Success response: single record
 const successResponse = (res, message, data, statusCode = 200) => {
   return res.status(statusCode).json({
     success: true,
@@ -6,7 +12,14 @@ const successResponse = (res, message, data, statusCode = 200) => {
   });
 };
 
-const successListResponse = (res, message, data, pagination, statusCode = 200) => {
+//Success list response: multiple records
+const successListResponse = (
+  res,
+  message,
+  data,
+  pagination,
+  statusCode = 200,
+) => {
   return res.status(statusCode).json({
     success: true,
     message,
@@ -15,6 +28,7 @@ const successListResponse = (res, message, data, pagination, statusCode = 200) =
   });
 };
 
+//Error response: error response
 const errorResponse = (res, message, code, details, statusCode) => {
   return res.status(statusCode).json({
     success: false,
